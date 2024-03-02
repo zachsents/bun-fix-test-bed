c58a6598c5279133696ad865e3f705797aa263c5
Zach Sents <zachsents@gmail.com>
2024-03-02 11:24:53 -0700

    Made minimal setup

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
-var admin = require("firebase-admin")
-
-var serviceAccount = require("path/to/serviceAccountKey.json")
+import admin, { type ServiceAccount } from "firebase-admin"
+import serviceAccount from "./service-account.json"
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount as ServiceAccount)
 })
+
+const db = admin.firestore()
+
+
+const docSnapshot = await db.collection("entries").doc("test").get()
+const docData = docSnapshot.data()
+
+console.log("Document ID:", docSnapshot.id)
+console.log("Document Data:", docData)
